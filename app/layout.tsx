0665c5f838c7d179423acf972095325da0693f4e
atefCodeEater <atefpoordehghan@gmail.com>
2024-08-28 19:45:03 +0330

    update env

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Provider } from "@/Provider";
-import { NextUIProvider } from "@nextui-org/react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
